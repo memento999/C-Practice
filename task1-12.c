0d1fade4914f738ad53c622767335e448c093bce
memento999 <janokakashvili@gmail.com>
2024-04-20 23:53:17 +0400

    extra 1–12 assignment

---
+++ task1-12.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+// Write a program that prints its input one word per line.
+int main()
+{
+    int c;
+
+    while ((c = getchar()) != '\n')
+    {
+        if (c == ' ' || c == '\n' || c == '\t')
+        {
+            putchar('\n');
+        }
+
+        putchar(c);
+    }
+    
+    printf("\n");
+
+    return 0;
+}
